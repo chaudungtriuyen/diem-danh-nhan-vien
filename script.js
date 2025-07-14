5de1696ff307a8ac3f34906e70fc16ed1d1965bf
chaudungtriuyen <162953086+chaudungtriuyen@users.noreply.github.com>
2025-07-14 14:06:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_URL_HERE";
+const API_URL = "https://script.google.com/u/0/home/projects/1suxbQhXB49XawxJGycG9hPMMHg_oadmpGsR7HPvtWEtLMVxIMg2iBW62/edit";
 let currentLeader = "";
 
 function dangNhap() {
